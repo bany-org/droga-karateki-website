d22f7d1381c71ea56de4cc0aef9325c1f794e119
Jakub Banasiak <soccerkid.bany@gmail.com>
2026-07-25 19:15:09 +0200

    clean up first fetch

--- app/page.tsx
+++ app/page.tsx
@@ -3,19 +3,17 @@ import Image from "next/image";
 export default async function Home() {
 
   const url = `${process.env.NEXT_PUBLIC_API_URL}/api/v1/hello`;
-
-  console.log(url);
   
   const response = await fetch(url);
-  const text = await response.text();
+  const text = await response.json();
   
-  console.log(text);
   
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
       <p>URL: {url}</p>
-      <pre>{text}</pre>
+      <pre>{text.message}</pre>
+      <pre>{text.details}</pre>
         <Image
           className="dark:invert"
           src="/next.svg"
